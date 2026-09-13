5bc9e004de912d10911ce1ef710563313ed7fcf2
Siva-Rajan-R <siva967763@gmail.com>
2026-09-13 11:58:26 +0530

    fixed the issues

--- background_worker.py
+++ background_worker.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from arq.connections import RedisSettings
 from infras.primary_db.services.customer_export_service import process_customer_export
 
